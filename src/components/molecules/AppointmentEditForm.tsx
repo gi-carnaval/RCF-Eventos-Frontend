5993e56b7c9a355cbe3a3459bfefcf73b7bcd444
Giovani Carnaval <gi.carnaval91@gmail.com>
2023-11-28 01:20:23 -0300

    refactor(appointments): refactoring appointments components

---
+++ src/components/molecules/AppointmentEditForm.tsx
@@ -0,0 +1,114 @@
+import { useForm } from 'react-hook-form'
+import Input from '../atoms/Input'
+// import { Button } from '../atoms/Button'
+import appointmentRepository from '@/src/repositories/AppointmentRepository'
+import InputErrorMessage from '../atoms/InputErrorMessage'
+import { useEffect, useState } from 'react'
+import { Button } from '../atoms/Button'
+import { usePopup } from '@/src/Hooks/usePopup'
+import { FullAppointmentProps } from '@/src/types/appointment'
+import dayjs from 'dayjs'
+
+export interface FormData {
+  appointmentTitle: string
+  date: string
+  locale: string
+  time: string
+}
+
+interface EditAppointmentFormProps {
+  appointmentId: string
+}
+
+export function EditAppointmentForm({
+  appointmentId,
+}: EditAppointmentFormProps) {
+  const [appointment, setAppointment] = useState<FullAppointmentProps>()
+
+  async function fetchAppointmentData(appointmentId: string) {
+    const res = await appointmentRepository.getAppointmentById(appointmentId)
+    setAppointment(res.data)
+  }
+  const { closePopup } = usePopup()
+
+  const {
+    register,
+    // handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<FormData>()
+
+  appointment && setValue('appointmentTitle', appointment?.appointmentTitle)
+  appointment && setValue('date', dayjs(appointment?.date).format('YYYY-MM-DD'))
+  appointment && setValue('locale', appointment?.locale)
+  appointment && setValue('time', appointment?.time)
+  // async function onSubmit(id: FormData) {
+  //   await appointmentRepository.getAppointmentById({
+  //     appointmentData,
+  //   })
+  // }
+
+  async function excludeAppointment() {
+    await appointmentRepository.deleteAppointment(appointmentId)
+    closePopup()
+  }
+
+  useEffect(() => {
+    fetchAppointmentData(appointmentId)
+  }, [appointmentId])
+
+  return (
+    <>
+      <div className="flex flex-row gap-6 py-6">
+        <div className="w-2/4">
+          <Input
+            id="apointmentTitle"
+            labelName="Título"
+            placeholder="Casamento..."
+            register={register('appointmentTitle', { required: true })}
+          />
+          {errors.appointmentTitle?.type === 'required' && (
+            <InputErrorMessage>Título é obrigatório *</InputErrorMessage>
+          )}
+        </div>
+        <div className="w-2/4">
+          <Input
+            id="appointmentDate"
+            labelName="Data"
+            type="date"
+            register={register('date', { required: true })}
+          />
+          {errors.date?.type === 'required' && (
+            <InputErrorMessage>Data é obrigatório *</InputErrorMessage>
+          )}
+        </div>
+      </div>
+      <div className="flex flex-row gap-6 py-6 -mt-6">
+        <div className="w-3/4">
+          <Input
+            id="apointmentLocale"
+            labelName="Local"
+            type="text"
+            register={register('locale', { required: true })}
+          />
+          {errors.locale?.type === 'required' && (
+            <InputErrorMessage>Local é obrigatório *</InputErrorMessage>
+          )}
+        </div>
+        <div className="w-1/4">
+          <Input
+            id="apointmentTime"
+            labelName="Horário"
+            type="time"
+            register={register('time', { required: true })}
+          />
+          {errors.time?.type === 'required' && (
+            <InputErrorMessage>Horário é obrigatório *</InputErrorMessage>
+          )}
+        </div>
+      </div>
+      {/* <Button onClick={() => handleSubmit(onSubmit)()}>Adicionar</Button> */}
+      <Button onClick={excludeAppointment}>Excluir</Button>
+    </>
+  )
+}
--- src/components/organism/AppointmentTable.tsx
+++ src/components/organism/AppointmentTable.tsx
@@ -1,13 +1,19 @@
-import { MdModeEditOutline } from 'react-icons/md'
-import TableTh from '../atoms/TableTh'
-import ButtonAdd from '../atoms/ButtonAdd'
-import CreateAppointmentForm from '../molecules/CreateAppointment'
 import { usePopup } from '@/src/Hooks/usePopup'
-import { AppointmentProps } from '@/src/types/event'
+import { EditAppointmentForm } from '../molecules/AppointmentEditForm'
+import dayjs from 'dayjs'
+import { FullAppointmentProps } from '@/src/types/appointment'
+import { TableTr } from '../atoms/TableTr'
+import { TableThead } from '../atoms/TableThead'
+import { TableCaption } from '../atoms/TableCaption'
+import { TableTdContent } from '../atoms/TableTdContent'
+import { ButtonHoverIcon } from '../atoms/ButtonHoverIcon'
+import { CreateAppointmentForm } from '../molecules/AppointmentCreateForm'
+import { TableTh } from '../atoms/TableTh'
+import { FaPlus } from 'react-icons/fa'
 
 interface AppointmentTableProps {
   id?: string
-  appointments?: AppointmentProps[]
+  appointments?: FullAppointmentProps[]
 }
 
 export default function AppointmentTable({
@@ -20,76 +26,66 @@ export default function AppointmentTable({
     title: 'Adicionar Compromisso',
     content: <CreateAppointmentForm eventId={id} />,
   }
-  const editAppointmentPopupContent = {
-    title: 'Editar Compromisso',
-    content: <CreateAppointmentForm eventId={id} />,
-  }
 
-  function editAppointment(id: string | undefined) {
-    alert(id)
+  function editAppointment(appointmentId: string) {
+    const editAppointmentPopupContent = {
+      title: 'Editar Compromisso',
+      content: <EditAppointmentForm appointmentId={appointmentId} />,
+    }
+    openPopup()
+    applyContent(editAppointmentPopupContent)
   }
 
   return (
     <div className="w-full flex flex-col px-40 py-12 items-center">
-      <table className="w-full ">
-        <caption className="text-lg font-bold text-gray-70">
-          Compromissos
-        </caption>
-        <thead>
-          <tr>
-            <TableTh>Título</TableTh>
-            <TableTh>Data</TableTh>
-            <TableTh className="w-2/5">Local</TableTh>
-            <TableTh>Dia da Semana</TableTh>
-            <TableTh>Horário</TableTh>
-          </tr>
-        </thead>
+      <table className="w-full border border-navy-40">
+        <TableCaption>Compromissos</TableCaption>
+        <TableThead>
+          <TableTh>Título</TableTh>
+          <TableTh>Data</TableTh>
+          <TableTh className="w-2/5">Local</TableTh>
+          <TableTh>Dia da Semana</TableTh>
+          <TableTh>Horário</TableTh>
+        </TableThead>
+
         <tbody>
           {appointments?.map((appointment) => {
             return (
-              <tr
+              <TableTr
                 key={appointment.id}
-                className="h-16 border-b-2 border-b-navy-60 px-6 hover:bg-navy-60 cursor-pointer"
                 onClick={() => {
-                  openPopup()
-                  applyContent(editAppointmentPopupContent)
+                  editAppointment(appointment.id)
                 }}
               >
-                <td className="w-1/5 text-base text-white-5 px-6">
+                <TableTdContent className="w-1/5">
                   {appointment.appointmentTitle}
-                </td>
-                <td className="w-1/5  text-base text-white-5 px-6">
-                  {appointment.date}
-                </td>
-                <td className="w-1/5  text-base text-white-5 px-6">
+                </TableTdContent>
+                <TableTdContent className="w-1/5">
+                  {dayjs(appointment.date).format('DD/MM/YYYY')}
+                </TableTdContent>
+                <TableTdContent className="w-1/5">
                   {appointment.locale}
-                </td>
-                <td className="w-1/5  text-base text-white-5 px-6 capitalize">
+                </TableTdContent>
+                <TableTdContent className="w-1/5 capitalize">
                   {appointment.dayOfWeek}
-                </td>
-                <td className="w-1/5  text-base text-white-5 px-6">
+                </TableTdContent>
+                <TableTdContent className="w-1/5">
                   {appointment.time}
-                </td>
-                <td
-                  onClick={() => {
-                    editAppointment(appointment.id)
-                  }}
-                  className="group/td fixed justify-center items-center hidden w-8 h-8 rounded-full -ml-2 cursor-pointer bg-navy-20 group-hover/tr:flex hover:w-20 hover:rounded-xl transition-all active:brightness-75 "
-                >
-                  <MdModeEditOutline />
-                  <span className="hidden group-hover/td:flex">Editar</span>
-                </td>
-              </tr>
+                </TableTdContent>
+              </TableTr>
             )
           })}
         </tbody>
       </table>
-      <ButtonAdd
+      <ButtonHoverIcon
+        icon={<FaPlus />}
         onClick={() => {
           openPopup()
           applyContent(addAppointmentPopupContent)
         }}
-      />
+      >
+        Adicionar Compromisso
+      </ButtonHoverIcon>
     </div>
   )
 }
