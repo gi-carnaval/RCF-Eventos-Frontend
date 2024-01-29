390a424538444b9f8afc0a62de88271d4ce5addf
Giovani Carnaval <gi.carnaval91@gmail.com>
2024-01-29 02:09:24 -0300

    fix(routing): fixing routing in electron

--- src/components/organism/Sidebar.tsx
+++ src/components/organism/Sidebar.tsx
@@ -1,8 +1,8 @@
 export default function Sidebar() {
   return (
     <nav className="fixed flex flex-col top-24 w-48 bg-navy-60 h-[calc(100vh_-_6rem)] border-r-2 border-r-navy-40 p-6">
-      <a href="./">Eventos</a>
-      <a href="./">Compromissos</a>
+      <a href="/">Eventos</a>
+      <a href="/">Compromissos</a>
     </nav>
   )
 }
